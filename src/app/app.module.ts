c59beabfddaedff086f794caf4804fadac2086e2
rawendra <rawendra@gmail.com>
2023-01-04 22:53:23 +0530

    updating the routing project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,29 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
 import { ServersComponent } from './servers/servers.component';
-import { CardComponent } from './card/card.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersService } from './servers/servers.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
+    HomeComponent,
+    UsersComponent,
     ServersComponent,
-    CardComponent
+    UserComponent,
+    EditServerComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,42 +1,18 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { ServersService } from './servers.service';
 
-let sometext = "this is awesome, never thought to c this";
 @Component({
-  // selector: '[app-servers]',
-  // selector: '.app-servers',
-  selector: "app-servers",
-  templateUrl: "./servers.component.html",
-  // template: `
-  //   <app-server></app-server>
-  //   <app-server></app-server>`,
-  styles: [
-    `
-      background-color: "pink";
-    `,
-  ],
-
-  styleUrls: ["./servers.component.css"],
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  clicked = false;
-  paratext = sometext;
-  inputText = "";
-  twoWayBindedString = "default";
-  constructor() {}
+  public servers: {id: number, name: string, status: string}[] = [];
 
-  onClick = () => {
-    this.clicked = !this.clicked;
-  };
+  constructor(private serversService: ServersService) { }
 
-  handleInput(event) {
-    console.log("event is getting called for input", event);
-    this.inputText = event.target.value;
-  }
   ngOnInit() {
-    console.log("ng-on-init is called");
-    // setInterval(() => {
-    //   this.clicked = !this.clicked;
-    //   this.paratext = this.clicked ? sometext : null;
-    // }, 1000);
+    this.servers = this.serversService.getServers();
   }
+
 }
